fba26e9cfd55edff265e7ce54cd944f4e81bb15b
smtdeveloper <sametakcatoki@gmail.com>
2023-01-30 21:40:21 +0300

    Core Layer Interfaces added

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T > where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GettAll(Expression<Func<T, bool>> expression);
+
+        // productRepository.where( x=> x.id = 5 ).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GettAllAsync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task Updatec(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+
+
+    }
+}
