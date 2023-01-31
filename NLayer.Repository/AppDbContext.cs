a912d38ee447e822afb7a24406becff2dd0244ab
smtdeveloper <sametakcatoki@gmail.com>
2023-01-31 16:16:07 +0300

    AppDbContext and Entity Configurations added

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using NLayer.Repository.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Product> Products{ get; set; }
+        public DbSet<ProductFeature> ProductFeatures{ get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //hepsini buluyor ve uyguluyor
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            ////tek tek yazıp uyguluyor
+            //modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            //modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+            //modelBuilder.ApplyConfiguration(new ProductFeatureConfigyration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ NLayer.Repository/Configurations/ProductConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(50);
+            builder.Property(x => x.Stock).IsRequired();
+
+            // $$$$$$$$$$$$$$$$,$$ --> (16 , 2) şeklinde. Toplam 18 VİRGÜLDEN(,)'den sonra 2 tane OLACAK. 
+            builder.Property(x => x.Price).IsRequired().HasColumnType("decimal(18,2)");
+
+
+
+            builder.ToTable("Products");
+
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(x => x.CategoryId);
+        }
+    }
+}
