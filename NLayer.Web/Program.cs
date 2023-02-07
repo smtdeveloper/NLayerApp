8de64a64ac17b464d141a1e83dfd71c72dc5aaa6
smtdeveloper <sametakcatoki@gmail.com>
2023-02-07 10:15:57 +0300

    ConnectionStrings ve program baglantıları yapıldı

--- NLayer.Web/Program.cs
+++ NLayer.Web/Program.cs
@@ -1,7 +1,34 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using NLayer.Web.Modeles;
+using Microsoft.EntityFrameworkCore;
+using NLayer.Repository;
+using System.Reflection;
+using NLayer.Service.Mapping;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAutoMapper(typeof(ProductProfile), typeof(CategoryProfile), typeof(ProductFeatureProfile));
+
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name);
+        //option.MigrationsAssembly("NLayer.Repository");
+    });
+});
+
+
+builder.Host.UseServiceProviderFactory
+    (new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => containerBuilder.RegisterModule(new RepoServiceModele()));
+
 
 var app = builder.Build();
 
