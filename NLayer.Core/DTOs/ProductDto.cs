679e8b8cc6e197998e8fc427408f3512078e5cdf
smtdeveloper <sametakcatoki@gmail.com>
2023-02-10 01:00:03 +0300

    games listelenmesi ok

--- NLayer.Core/DTOs/ProductDto.cs
+++ NLayer.Core/DTOs/ProductDto.cs
@@ -3,6 +3,8 @@
     public class ProductDto : BaseDto
     {
         public string Name { get; set; }
+        public string Declaration { get; set; }
+        public string Image { get; set; }
         public int Stock { get; set; }
         public decimal Price { get; set; }
         public int CategoryId { get; set; }
